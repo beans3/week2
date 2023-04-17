e0b486429f6d7d1e9aa0ee3dad1d8a9d31e46c43
anahi <anahi@mt.co.kr>
2023-04-17 14:54:50 +0900

    2023-04-17 리액트 간단히 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [title, updateTitle] = useState(['안녕하세요.', '남자 코트 정보', '강아지 코트 정보']);
+  let [likeCount, updateLikeCount] = useState(0);
+
+  function updateTitle(title) {
+    title[0] = '여자 코트 정보' ;
+  }
+ 
+  // state 쓰는 이유 : 일반 변수는 내용이 변경 되어도 html 업데이트가 안됨
+  // distructuring 문법
+  // let [a, c] = [1, 2]; // a = 1; c = 2;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4 style={{color: 'red', fontSize: '20px'}}>블로그임</h4>
+      </div>
+      <h4 id={post}>{post}</h4>
+      <button onClick={ () => { updateTitle('여자 코트 정보') } }>최신순</button>
+      <div className='list'>
+        <h4>{ title[0] } <span onClick={ () => { updateLikeCount(likeCount+1) } }>👍</span>{ likeCount }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
