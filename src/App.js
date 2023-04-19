452ed5c1c02af292e1e452951708e28a6aacdb37
anahi <anahi@mt.co.kr>
2023-04-19 10:08:09 +0900

    2023-04-19 map으로 게시물 리스트 만들기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 /* eslint-disable */
-
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-
-  let post = '강남 우동 맛집';
-  let [title, updateTitle] = useState(['안녕하세요.', '남자 코트 정보', '강아지 코트 정보']);
-  let [likeCount, updateLikeCount] = useState(0);
+  const { v4: uuidv4 } = require('uuid');
+  //연습용 배열
+  const posts = [
+    { id: uuidv4(), title: "첫 번째 게시물", date: "2022-01-01", likes: 5, content: "첫 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "두 번째 게시물", date: "2022-02-14", likes: 10, content: "두 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "세 번째 게시물", date: "2022-03-10", likes: 2, content: "세 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "네 번째 게시물", date: "2022-04-20", likes: 8, content: "네 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "다섯 번째 게시물", date: "2022-05-06", likes: 13, content: "다섯 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "여섯 번째 게시물", date: "2022-06-30", likes: 0, content: "여섯 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "일곱 번째 게시물", date: "2022-07-12", likes: 3, content: "일곱 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "여덟 번째 게시물", date: "2022-08-05", likes: 6, content: "여덟 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "아홉 번째 게시물", date: "2022-09-23", likes: 4, content: "아홉 번째 게시물 내용입니다." },
+    { id: uuidv4(), title: "열 번째 게시물", date: "2022-10-31", likes: 1, content: "열 번째 게시물 내용입니다." }
+  ];
+  let post = '리액트 맛집';
+  let [postList, updatePost] = useState(posts);
   let [modal, showModal] = useState(false);
+  let [modalId, setModalId] = useState('');
+
+  const updateLikeCount = (id) => {
+    // post의 내용을 update한 뒤에
+    // updatePost 호출하여 적용
+    const updatedPosts = postList.map(post => {
+      if (post.id === id) {
+        return {...post, likes: post.likes + 1};
+      } else {
+        return post;
+      }
+    });
+    updatePost(updatedPosts);
+  }
+
+  // 포스트 클릭 시 모달 창이 열리게 하는 함수
+  const openPost = (id) =>  {
+    // 모달창을 클릭할 때 id 값을 넘겨서 ModalId 라는 state 값이 id 값으로 바뀌도록 함
+    setModalId(id);
+    if (modal == true) {
+      showModal(false);
+    } else {
+      showModal(true);
+    }
+  }
  
   // state 쓰는 이유 : 일반 변수는 내용이 변경 되어도 html 업데이트가 안됨
   // distructuring 문법
@@ -17,46 +53,58 @@ function App() {
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4 style={{color: 'red', fontSize: '20px'}}>블로그임!!!</h4>
+        <h4 style={{color: 'red', fontSize: '20px'}}>Nepflix</h4>
       </div>
       <h4 id={post}>{post}</h4>
-      <button onClick={ () => {
-        let copy = [...title];  //독립적인 복사본을 만들어서 수정해야 함(shallow copy, deep copy 찾아보기)
-        copy[0] = '여자 코트 추천';
-        updateTitle(copy);
+      <button className='btn' onClick={ () => {
+        let copy = [...postList];  //독립적인 복사본을 만들어서 수정해야 함(shallow copy, deep copy 찾아보기)
+        copy[0].title = '여자 코트 추천';
+        updatePost(copy);
 
         //reference data type
         //결과가 모두 false인데 왜 모두 false일까... 첫번째는 true 아님?
-        console.log(copy == title)
-        console.log(copy === title)
+        console.log(copy == copy[0].title)
+        console.log(copy === copy[0].title)
+
+        let array1 = [2, 3, 4];
+        let array2 = array1.map(function(callback) {
+          return callback * 10;
+        })
+        console.log(array2);
+        array1.map(function() {
+          console.log(1);
+        })
+        array1.map(function(callback) {
+          console.log(callback);
+        })
+        
       } }>첫번째 제목 수정</button>
-      <button onClick={ () => {
-        let copy2 = [...title];
-        copy2.sort();
-        updateTitle(copy2);
+      <button className='btn' onClick={ () => {
+        // sort() 메서드에 전달된 비교 함수는 두 개의 요소를 비교하고,
+        // 반환값이 0보다 작으면 첫 번째 인수를 앞으로,
+        // 0보다 크면 두 번째 인수를 앞으로 정렬합니다.
+        // 이때, a와 b는 각각 두 요소를 나타내며,
+        // a.title과 b.title은 두 요소의 title 속성을 나타냅니다.
+        let copy2 = [...postList].sort((a, b) => a.title.localeCompare(b.title));
+        updatePost(copy2);
       } }>가나다순으로 정렬</button>
-      <div className='list'>
-        <h4>{ title[0] } <span onClick={ () => { updateLikeCount(likeCount+1) } }>👍</span>{ likeCount }</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{ title[1] }</h4>
-        <p>2월 17일 발행!</p>
-      </div>
-      <div className='list' onClick={ () =>  {
-          if (modal == false) {
-            showModal(true);
-          } else {
-            showModal(false);
-          }
-        }}>
-        <h4>{ title[2] }</h4>
-        <p>2월 17일 발행</p>
-      </div>
       {
-        modal == false ? null : <Modal/>
+        // map으로 배열 리스트 반복
+        postList.map(posts => (
+            <div key={posts.id}>
+              <div className='list' onClick={ () => openPost(posts.id) }>
+                <h4>{ posts.title } <span onClick={ () => { updateLikeCount(post.id) } }>👍</span>{ posts.likes }</h4>
+                <p>{ posts.date } 발행</p>
+              </div>
+            </div>
+          )
+        )
       }
-      <PageList/>
+      {
+        // postList 중에 id가 modalId와 같은 요소만 props로 보냄
+        modal == false ? null : <Modal posts={ postList.find(post => post.id === modalId) } updatePost={updatePost} color={ 'skyblue' }/>
+      }
+      <More/>
     </div>
   );
 }
@@ -65,7 +113,7 @@ function App() {
 // 1. HTML, CSS로 변경 후 디자인 미리 완성
 // 2. UI의 현재 상태를 state로 저장
 // 3. state에 따라 UI가 어떻게 보일지 작성
-function Modal() {
+function Modal(props) {
   return (
     // Fragment 문법
     // componenet 만들어 html 축약
@@ -73,31 +121,38 @@ function Modal() {
     // 2. 큰 페이지들
     // 3. 자주 변경되는 것들
     // App()의 state 사용 불가
+    
     <>
-      <div className='modal'>
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
+      <div className='modal' id={props.posts.id} style={{backgroundColor: props.color}}>
+        <h4>{ props.posts.title }</h4>
+        <p>{ props.posts.date }</p>
+        <p>{ props.posts.content }</p>
+        <button className='btn' onClick={ () => {
+          let copy = [...props.postList];
+          copy.find(post => post.id === props.modalId).title = '제목 바꾸기';
+          props.updatePost(copy);
+        } }>글수정</button>
       </div>
-      <div></div>
+      {/* <div className='modal' id={props.posts[0].id} style={{backgroundColor: props.color}}>
+        <h4>{ props.posts[0].title }</h4>
+        <p>{ props.posts[0].date }</p>
+        <p>{ props.posts[0].content }</p>
+        <button className='btn' onClick={ () => {
+          let copy = [...props.posts];
+          copy[0].title = '제목 바꾸기';
+          props.updatePost(copy);
+        } }>글수정</button>
+      </div> */}
     </>
   )
 }
 
 // 이렇게 사용 가능
-const PageList = () => {
+const More = () => {
   return (
-    <>
-      <span>이전</span>
-      <ul className="page-list">
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-      </ul>
-      <span>다음</span>
-    </>
+    <div>
+      <button className='btn'>더보기</button>
+    </div>
   )
 }
 
